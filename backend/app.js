206e524fdbdf3f8e8f068c8e276d9b69159b9983
francescopiscaglia <piscagliafrancesco@gmail.com>
2025-01-09 10:47:52 +0100

    add db connection

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
+const DBConnection = require("./db/connection.js");
 
 const PORT = process.env.PORT;
 const LOCALHOST = process.env.LOCALHOST;
 
+// body parse middleware
+app.use(express.json());
+
+// start the server
 app.listen(PORT, () => {
     console.log(`Server is running at ${LOCALHOST}:${PORT}`);
 });
 
+// index
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
