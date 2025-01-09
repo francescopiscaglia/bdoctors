0aa2be9b8e60b16d905a3393966fb9d99b5c0b4a
francescopiscaglia <piscagliafrancesco@gmail.com>
2025-01-09 11:04:06 +0100

    add index

--- backend/controller/doctorsController.js
+++ backend/controller/doctorsController.js
@@ -1,7 +1,17 @@
 const DBConnection = require("../db/connection.js");
 
 const index = (req, res) => {
-    res.send("Hello World!");
+
+    const sql = `SELECT * FROM doctors`;
+
+    DBConnection.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Error retrieving doctors from database");
+        } else {
+            res.status(200).json(result);
+        }
+    });
 };
 
 const show = (req, res) => {
