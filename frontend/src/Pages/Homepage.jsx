a6b42fa4798085aa984b154d5e83e24d3412baa8
dariopalmesi <dariopalmesi88@gmail.com>
2025-01-13 18:19:04 +0100

    add map doctors

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,22 +1,40 @@
-export default function Homepage() {
+import { useState, useEffect } from "react";
 
-    fetch('http://localhost:3008/api/doctors')
-        .then(res => res.json())
-        .then(data => {
-            console.log('data', data)
-        })
+export default function Homepage() {
+    const [doctors, setDoctors] = useState();
+    useEffect(() => {
+        fetch('http://localhost:3008/api/doctors')
+            .then(res => res.json())
+            .then(data => {
+                console.log('data', data)
+                setDoctors(data)
+            })
+    }, [])
+
+
 
     return (
         <>
             <div className="container">
                 <div className="row">
-                    <div className="col">
-                        <div className="card">
-                            <div className="card-body">
-
+                    {doctors ? doctors.map(doctor => (
+                        <div className="col" key={doctor.id}>
+                            <div className="card">
+                                <div className="card-body">
+                                    <p>{doctor.name}</p>
+                                    <p>{doctor.last_name}</p>
+                                    <p>{doctor.department}</p>
+                                    <p>{doctor.email}</p>
+                                    <p>{doctor.phone_number}</p>
+                                    <p>{doctor.addres}</p>
+                                    <p>{doctor.description}</p>
+                                    <p>{doctor.cv}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    )) : (
+                        <p>error</p>
+                    )}
                 </div>
             </div>
         </>
