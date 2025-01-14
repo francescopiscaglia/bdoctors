248cd77d9c550e73ae12bfc1a86fc1c94c8d8c0e
francescopiscaglia <piscagliafrancesco@gmail.com>
2025-01-14 11:06:40 +0100

    edit footer

--- frontend/src/Components/AppFooter.jsx
+++ frontend/src/Components/AppFooter.jsx
@@ -1,10 +1,13 @@
 export default function AppFooter() {
+    const currentYear = new Date().getFullYear();
+
     return (
         <>
-            <footer>
-                <p>Lorem ipsum dolor sit amet.</p>
+            <footer className="text-white py-4">
+                <div className="container d-flex justify-content-center align-items-center">
+                    <p style={{ fontSize: '12px' }}>&copy; {currentYear} <span>BDoctor</span>. All rights reserved.</p>
+                </div>
             </footer>
-
         </>
-    )
+    );
 }
