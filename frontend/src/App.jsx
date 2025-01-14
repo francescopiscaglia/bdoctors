1aa122f2d2c1f8efd1bdcf1d7f9d588444d139d3
JacopoTrincanato <trincanatoj@gmail.com>
2025-01-14 11:32:52 +0100

    create form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import DefaultLayout from './Layout/DefaultLayout'
 import Homepage from './Pages/Homepage'
+import AddDoctor from './Pages/AddDoctor'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route element={<DefaultLayout />}>
                         <Route path='/' element={<Homepage />} />
+                        <Route path='/add' element={<AddDoctor />}></Route>
                     </Route>
                 </Routes>
 
